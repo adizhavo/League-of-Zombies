ace405945ccae314d59f7fba7a9979844c7c1978
Adi Zhavo <adi@gram.gs>
2016-07-30 22:41:30 +0300

    Added ground input

---
+++ Assets/GroundInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundInput : MonoBehaviour {
+
+    public delegate void GroundTouch(Vector3 groudPos);
+    public static event GroundTouch OnGroundTouch;
+
+    [SerializeField] private LayerMask groundMask;
+
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // are we clicking a UI object ?
+
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100, groundMask))
+            {
+                Debug.Log(hit.point);
+
+                if (OnGroundTouch != null)
+                    OnGroundTouch(hit.point);
+            }
+        }
+	}
+}
