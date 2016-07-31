20af8b37bbffae1b008f67c4889af4eb1d487a8c
Adi Zhavo <adi@gram.gs>
2016-07-31 15:43:50 +0300

    Added input config

---
+++ Assets/Scripts/InputConfig.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class InputConfig
+{
+    #if UNITY_EDITOR
+
+    public static bool TouchDown()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    public static bool TouchUp()
+    {
+        return Input.GetMouseButtonUp(0);
+    }
+
+    public static bool TouchDrag()
+    {
+        return Input.GetMouseButton(0);
+    }
+
+    public static bool CastSpell()
+    {
+        return false;
+    }
+
+    #endif
+}
